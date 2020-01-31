1f98764c04653e488f9608731eec68a42056045c
pacdong <8000pac@naver.com>
2020-01-31 13:59:50 +0900

    2020-01-31 / Nomard InstaClone #12.2 Auth Components part Two (No Error)

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
+import useInput from "../../hooks/useInput";
 
 const View = styled.View`
   justify-content: center;
@@ -12,9 +13,15 @@ const View = styled.View`
 const Text = styled.Text``;
 
 export default () => {
+  const emailInput = useInput("");
+
   return (
     <View>
-      <AuthInput value="" placeholder="Email" keyboardType="email-address" />
+      <AuthInput
+        {...emailInput}
+        placeholder="Email"
+        keyboardType="email-address"
+      />
       <AuthButton onPress={() => null} text="Log In" />
     </View>
   );
