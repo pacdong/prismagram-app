6da6394c1c65048296c3feb902d789848d96a2b0
pacdong <8000pac@naver.com>
2020-02-04 11:05:55 +0900

    2020-02-04 / InstaCLone # 15. Post Component Header

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
 import { useQuery } from "react-apollo-hooks";
+import Post from "../../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -62,7 +63,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Hello</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };
